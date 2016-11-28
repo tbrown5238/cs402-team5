414baee14db1ca4ec7283d0b7b23767b5a17cf91
Jason <jderric2@vols.utk.edu>
2016-11-28 01:47:17 -0500

    fixed ints to double in q_learning.cpp and added q_learning.h

---
+++ sim_v01/q_learning.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void initialize_environment();
+void initialize_brain();
+int choose_action(int);
+double max_next(int);
+int Episode(int);
+void print_32();
